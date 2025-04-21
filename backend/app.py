a7a7b8afeee9a752e0910591117fbc1687c577a3
Syed Umer Tariq <umart200@gmail.com>
2025-04-21 10:16:23 -0500

    product api created

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 from routes.customers import customers_bp
-# from routes.products import products_bp
+from routes.products import products_bp
 # from routes.orders import orders_bp
 # from routes.orderDetails import order_details_bp
 
@@ -11,7 +11,7 @@ CORS(app)
 
 # Register routes with prefixes
 app.register_blueprint(customers_bp, url_prefix="/api/customers")
-# app.register_blueprint(products_bp, url_prefix="/api/products")
+app.register_blueprint(products_bp, url_prefix="/api/products")
 # app.register_blueprint(orders_bp, url_prefix="/api/orders")
 # app.register_blueprint(order_details_bp, url_prefix="/api/orderdetails")
 
